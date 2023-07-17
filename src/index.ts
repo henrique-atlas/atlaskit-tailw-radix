4909c4e36c65b20a85e58c870bc4f9da7a23d7a3
Henrique Hallmann <henriquev@atlashxm.com>
2023-07-17 09:26:32 -0300

    removed not used comps

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,5 @@
 import {TwButton} from "./components/Button/TwButton";
-import Button from "./components/Button";
 import RadixSwitch from "./components/Switch/RadixSwitch";
 import './main.css'
 
-export { Button, RadixSwitch, TwButton };
+export { RadixSwitch, TwButton };
